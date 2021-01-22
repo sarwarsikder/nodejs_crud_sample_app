20bc11b0118a10c43480db41827db5bc50574421
sarwar sikder <sarwar@workspaceit.com>
2021-01-22 13:48:15 +0600

    add serverjs
    Update run script

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000,function(){
+    console.log('listening on 3000')
+})
+
+app.get('/',function(req, res){
+    res.sendFile(__dirname + '/index.html')
+    // res.send("Hello World!")
+})
+
